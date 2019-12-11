806f6075e9de98e371fd810a4c9e0916c50108e5
Phillip Dunham <phillipwwd@gmail.com>
2019-12-11 11:57:02 -0800

    adding click handlers

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 // var jumbo-head = document.getelementbyid();
 // empty url: q tagged part is sample from NYT
-var apiurl = https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet;
-var apiurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet&" + term;
+// apiurl = https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet;
+var apiurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet&q=";
+
 // click handler for the text input from the search field
 $().on("click", function(){
-    var term = $(searchclassname).val();
+    var term = $(searchclassname).val(); // generic name
+    apiurl += term;   
+})
+// click handler for the numer of articles wanted
+$().on("click", function(){
+    var num = $(numberclassname).val(); // generic name
+})
 
+// click handler for the start year
+$().on("click", function(){
+    var start = $(startclassname).val(); // generic name   
 })
