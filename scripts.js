8715dcd0de0c89d524c7590257840217d4cd313a
chrisp-dev <chris.paradeise@gmail.com>
2019-12-11 11:55:26 -0800

    Added script to HTML and scaffolded some Javascript

--- scripts.js
+++ scripts.js
@@ -1,9 +1,28 @@
-// var jumbo-head = document.getelementbyid();
-// empty url: q tagged part is sample from NYT
-var apiurl = https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet;
-var apiurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet&" + term;
-// click handler for the text input from the search field
-$().on("click", function(){
-    var term = $(searchclassname).val();
+$(document).ready(function () {
+    // on page load -> do nothing? wait for button click
 
+    // we want the clear and submit buttons
+    let clearBtn = $('#clear');
+    let searchBtn = $('#search');
+
+    clearBtn.on('click', clearFormAndTopArticles);
+    searchBtn.on('click', searchForArticles);
+
+    function clearFormAndTopArticles() {
+        event.preventDefault();
+        // clear inputs
+        // clear top articles div
+    }
+
+    function searchForArticles() {
+        event.preventDefault();
+    }
+
+    // empty url: q tagged part is sample from NYT
+    var apiurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=ibDCUlr0NbVM7oh6s6RIiAubOTmpKaet&q=";
+    // click handler for the text input from the search field
+    $().on("click", function () {
+        var term = $(searchclassname).val();
+
+    })
 })
